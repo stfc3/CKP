a4ed45ae46f4c1c0f64b5dcff329062ea42ed903
dongvg <31316424+dongvg@users.noreply.github.com>
2017-11-06 23:34:04 +0700

    da

--- document/ScriptDB.sql
+++ document/ScriptDB.sql
@@ -284,7 +284,7 @@ INSERT INTO users (user_name, full_name, password) VALUE ('admin','Admin', 'BIiU
 
 DELIMITER $$
 CREATE DEFINER=`ckp`@`%` PROCEDURE `calculator_revenue`(IN p_construction_id BIGINT, IN p_pump_type INT, 
-IN p_location_type INT, IN p_location_id BIGINT, IN p_quantity DOUBLE, IN p_num_shift INT)
+IN p_location_type INT, IN p_location_id BIGINT, IN p_quantity DOUBLE, IN p_num_wait INT, IN p_num_switch INT, IN p_auto_convert INT)
 BEGIN
 #biến dừng lặp cursor
 DECLARE done INT DEFAULT FALSE;
@@ -293,50 +293,74 @@ DECLARE total_revenue DOUBLE DEFAULT 0;
 DECLARE v_m3_revenue DOUBLE DEFAULT 0;
 DECLARE v_shift_revenue DOUBLE DEFAULT 0;
 DECLARE v_wait_revenue DOUBLE DEFAULT 0;
+DECLARE v_switch_revenue DOUBLE DEFAULT 0;
 DECLARE v_location_revenue DOUBLE DEFAULT 0;
+
+#Biến công thức tính
+DECLARE v_formula_wait VARCHAR(50);
+DECLARE v_formula_switch VARCHAR(50);
+DECLARE v_formula_m3 VARCHAR(50);
+DECLARE v_formula_shift VARCHAR(50);
+DECLARE v_formula_loc_m3 VARCHAR(50);
+DECLARE v_formula_loc_shift VARCHAR(50);
+DECLARE v_formula VARCHAR(500) DEFAULT '';
+
 #Biến select trong price
-DECLARE v_price_m3, v_price_shift, v_price_wait, v_price_location, v_price_location_shift, v_convert_value DOUBLE;
+DECLARE v_price_m3, v_price_shift, v_price_wait, v_price_switch, v_price_location, v_price_location_shift, v_convert_value DOUBLE;
 DECLARE v_convert_type INT;
 DECLARE v_price_id, v_location_min, v_location_max BIGINT;
 
 DECLARE v_is_shift DOUBLE DEFAULT 0;
 
 DECLARE c_data CURSOR FOR 
-SELECT p.price_id, p.price_m3, p.price_shift, p.price_wait,  p.convert_type, p.convert_value 
+SELECT p.price_id, p.price_m3, p.price_shift, p.price_wait, p.price_switch,  p.convert_type, p.convert_value 
 FROM construction ct, contracts c, prices p 
 WHERE  ct.contract_id=c.contract_id AND c.contract_id=p.contract_id AND ct.status=1 AND c.status=1 AND p.status=1
-AND ct.construction_id=p_construction_id AND p.pump_type=p_pump_type;
+AND ct.construction_id=p_construction_id AND p.pump_type=p_pump_type AND p.price_type=1;
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 OPEN c_data;		
         START_LOOP: LOOP
-			FETCH c_data INTO v_price_id, v_price_m3, v_price_shift, v_price_wait, v_convert_type, v_convert_value;
+			FETCH c_data INTO v_price_id, v_price_m3, v_price_shift, v_price_wait, v_price_switch, v_convert_type, v_convert_value;
 			IF done THEN
 				LEAVE START_LOOP;
 			END IF;
             BEGIN
 				#Nếu có ca chờ --> Tính giá ca chờ
-				IF(p_num_shift IS NOT NULL) THEN
-                    SET v_wait_revenue=p_num_shift*v_price_wait;
+				IF(p_num_wait IS NOT NULL) THEN
+					IF(v_price_wait IS NOT NULL) THEN
+						SET v_wait_revenue=p_num_wait*v_price_wait;
+						SET v_formula_wait=concat('(',p_num_wait,'*',v_price_wait,')');
+                    END IF;
+				END IF;
+                #Nếu có ca chuyển chân --> Tính giá ca chuyển chân
+				IF(p_num_switch IS NOT NULL) THEN
+					IF(v_price_switch IS NOT NULL) THEN
+						SET v_switch_revenue=p_num_switch*v_price_switch;
+						SET v_formula_switch=concat('(',p_num_switch,'*',v_price_switch,')');
+					END IF;
 				END IF;
 				#Tính giá bơm
                 #Nếu loại quy đổi là m3
                 IF(v_convert_type=1) THEN
 					SET v_is_shift=0;
 					#Nếu khối lượng < khối lượng giới hạn quy đổi
-                    IF(p_quantity< v_convert_value) THEN
+                    IF(p_quantity< v_convert_value AND p_auto_convert=1) THEN
 						SET p_quantity = v_convert_value;
                     END IF;
                     SET v_m3_revenue =p_quantity*v_price_m3;
+                    SET v_formula_m3=concat('(',p_quantity,'*',v_price_m3,')');
 				#Nếu tính loại quy đổi là ca
 				ELSE
 					
 					#Nếu khối lượng < khối lượng giới hạn quy đổi
-                    IF(p_quantity< v_convert_value) THEN
+                    IF(p_quantity< v_convert_value AND p_auto_convert=1) THEN
                         SET v_shift_revenue =v_price_shift;
                         SET v_is_shift=1;
+                        SET v_formula_shift=concat('(',v_price_shift,')');
 					ELSE
 						SET v_is_shift=0;
 						SET v_m3_revenue =p_quantity*v_price_m3;
+                        SET v_formula_m3=concat('(',p_quantity,'*',v_price_m3,')');
                     END IF;
 				END IF;
                 #Tính giá vị trí
@@ -347,15 +371,19 @@ OPEN c_data;
                 IF(@lo_value<@lo_min) THEN
 					IF(v_is_shift=0) THEN
 						SET v_location_revenue= (@lo_min-@lo_value)*@price_location*p_quantity;
+                        SET v_formula_loc_m3=concat('((',@lo_min,'-',@lo_value,')','*',@price_location,'*',p_quantity,')');
 					ELSE
 						SET v_location_revenue= (@lo_min-@lo_value)*@price_location_shift;
+                        SET v_formula_loc_shift=concat('((',@lo_min,'-',@lo_value,')','*',@price_location_shift,')');
                     END IF;
                 ELSE
 					IF(@lo_value>@lo_max) THEN
 						IF(v_is_shift=0) THEN
 							SET v_location_revenue= (@lo_value-@lo_max)*@price_location*p_quantity;
+                            SET v_formula_loc_m3=concat('((',@lo_value,'-',@lo_max,')','*',@price_location,'*',p_quantity,')');
 						ELSE
 							SET v_location_revenue= (@lo_value-@lo_max)*@price_location_shift;
+                            SET v_formula_loc_shift=concat('((',@lo_value,'-',@lo_max,')','*',@price_location_shift,')');
 						END IF;   
 					END IF;
 				END IF;
@@ -364,16 +392,53 @@ OPEN c_data;
                     SELECT price_location, price_location_shift INTO @price_location_other, @price_location_shift_other  FROM price_location p WHERE p.price_id=v_price_id AND p.location_type=p_location_type;
                     IF(v_is_shift=0) THEN
 						SET v_location_revenue=v_location_revenue+@price_location_other*p_quantity;
+                        SET v_formula_loc_m3=concat(v_formula_loc_m3,'+','(',price_location_other,'*',p_quantity,')');
 					ELSE
 						SET v_location_revenue=v_location_revenue+@price_location_shift_other;
+                        SET v_formula_loc_shift=concat(v_formula_loc_shift,'+','(',price_location_shift_other,')');
 					END IF;  
                 END IF;
 			END;
 		END LOOP START_LOOP; 
-        SET total_revenue=v_location_revenue+v_m3_revenue+v_wait_revenue+v_shift_revenue;
-        SELECT total_revenue, 'Thành công' description;
-END
-DELIMITER $$
+        # Tổng doanh thu
+        IF(v_location_revenue IS NOT NULL) THEN
+			SET total_revenue=total_revenue+v_location_revenue;
+        END IF;
+        IF(v_m3_revenue IS NOT NULL) THEN
+			SET total_revenue=total_revenue+v_m3_revenue;
+        END IF;
+        IF(v_wait_revenue IS NOT NULL) THEN
+			SET total_revenue=total_revenue+v_wait_revenue;
+        END IF;
+        IF(v_shift_revenue IS NOT NULL) THEN
+			SET total_revenue=total_revenue+v_shift_revenue;
+        END IF;
+        IF(v_switch_revenue IS NOT NULL) THEN
+			SET total_revenue=total_revenue+v_switch_revenue;
+        END IF;
+        # Công thức
+        IF(v_formula_m3 IS NOT NULL) THEN
+			SET v_formula=concat(v_formula_m3);
+        END IF;
+        IF(v_formula_shift IS NOT NULL) THEN
+			SET v_formula=concat(v_formula,'+',v_formula_shift);
+        END IF;
+        IF(v_formula_wait IS NOT NULL) THEN
+			SET v_formula=concat(v_formula,'+',v_formula_wait);
+        END IF;
+        IF(v_formula_switch IS NOT NULL) THEN
+			SET v_formula=concat(v_formula,'+',v_formula_switch);
+        END IF;
+        IF(v_formula_loc_m3 IS NOT NULL) THEN
+			SET v_formula=concat(v_formula,'+',v_formula_loc_m3);
+        END IF;
+        IF(v_formula_loc_shift IS NOT NULL) THEN
+			SET v_formula=concat(v_formula,'+',v_formula_loc_shift);
+        END IF;
+        SELECT total_revenue as revenue, v_formula as formula;
+END$$
+DELIMITER ;
+
 
 DELIMITER $$
 CREATE PROCEDURE `calculator_quantity`(in p_bill_detail_id bigint)
@@ -386,9 +451,12 @@ DECLARE v_quantity_limit INT DEFAULT 60;
 DECLARE v_percent double DEFAULT 0.25; # 25%
 
 
-SELECT max_staff, is_far, quantity_convert, CASE WHEN quantity_approve IS NOT NULL THEN quantity_approve ELSE quantity END quantity 
+SELECT bd.max_staff, c.is_far, bd.quantity_convert, CASE WHEN bd.quantity_approve IS NOT NULL THEN bd.quantity_approve ELSE bd.quantity END quantity
 INTO @max_staff, @is_far, @quantity_convert, @quantity
-FROM bill_detail WHERE bill_detail_id=p_bill_detail_id AND status=1;
+FROM bill_detail bd, bills b ,construction c
+WHERE bd.bill_id=b.bill_id AND b.status=1
+AND b.construction_id=c.construction_id AND c.status=1
+AND bd.bill_detail_id=p_bill_detail_id AND bd.status=1;
 # Lây tổng số công nhân thực hiện bơm
 SELECT COUNT(*) INTO @staff_total FROM quantity_staff WHERE bill_detail_id=p_bill_detail_id;
 
@@ -404,12 +472,13 @@ IF (@is_far = 1) THEN
 END IF;
 #Công thức tính khối lượng: (Khối lượng * số lượng công nhân tối đa)/ số lượng công nhân thực hiện
 SET v_quantity=v_quantity*@max_staff/ @staff_total;
-SET SQL_SAFE_UPDATES=0;
+#SET SQL_SAFE_UPDATES=0;
 UPDATE  quantity_staff SET quantity=v_quantity, create_date = now() WHERE bill_detail_id=p_bill_detail_id;
-SET SQL_SAFE_UPDATES=1;
+#SET SQL_SAFE_UPDATES=1;
 select v_quantity, @is_far,@max_staff, @staff_total;
 END$$
 DELIMITER ;
+
 
 
 INSERT INTO params (param_key, param_value, param_name) VALUES ('BANK',1,'Vietcombank');
