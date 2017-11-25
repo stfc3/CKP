1bfc69087cd3b912af3a1743e2376b8dab42bfd7
viettx <viettx.dev@gmail.com>
2017-11-25 09:48:24 +0700

    update approve

--- CKPManagement/src/main/java/com/dvd/ckp/controller/BillsViewController.java
+++ CKPManagement/src/main/java/com/dvd/ckp/controller/BillsViewController.java
@@ -37,193 +37,202 @@ import org.zkoss.zul.Rows;
 
 public class BillsViewController extends GenericForwardComposer<Component> {
 
-    /**
-     *
-     */
-    private static final Logger LOGGER = Logger.getLogger(BillsViewController.class);
-    private static final long serialVersionUID = -3785065052690864441L;
-    @WireVariable
-    protected BillsServices billsServices;
+	/**
+	 *
+	 */
+	private static final Logger LOGGER = Logger.getLogger(BillsViewController.class);
+	private static final long serialVersionUID = -3785065052690864441L;
+	@WireVariable
+	protected BillsServices billsServices;
 
-    @Wire
-    private Grid gridBillsDetail;
-    @Wire
-    private Intbox roleApprove;
+	@Wire
+	private Grid gridBillsDetail;
+	@Wire
+	private Intbox roleApprove;
 
-    // Danh sach hoa don chi tiet
-    private List<BillViewDetail> lstBillDetail;
+	// Danh sach hoa don chi tiet
+	private List<BillViewDetail> lstBillDetail;
 
-    private List<BillsDetail> lstDataBill;
+	private List<BillsDetail> lstDataBill;
 
-    private List<Bills> lstBill;
+	private List<Bills> lstBill;
 
-    // Model grid in window bill detail
-    ListModelList<BillViewDetail> listDataModelDetail;
+	// Model grid in window bill detail
+	ListModelList<BillViewDetail> listDataModelDetail;
 
-    // Vi tri cac column trong grid bills detail
-    private Longbox txtBillID;
-    private String billDetailId;
+	// Vi tri cac column trong grid bills detail
+	private Longbox txtBillID;
+	private String billDetailId;
 
-    private Window windowViewBillDetail;
+	private Window windowViewBillDetail;
 
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		// TODO Auto-generated method stub
+		super.doAfterCompose(comp);
+		Long billId = txtBillID.getValue();
+		billsServices = (BillsServices) SpringUtil.getBean(SpringConstant.BILL_SERVICES);
+		List<BillViewDetail> lstData = billsServices.getDataView(billId);
+		List<BillsDetail> lstDetail = billsServices.getBillDetail(billId);
+		lstBillDetail = new ArrayList<>();
+		lstDataBill = new ArrayList<>();
+		if (lstData != null && !lstData.isEmpty()) {
+			lstBillDetail.addAll(lstData);
+		}
+		if (lstDetail != null && !lstDetail.isEmpty()) {
+			lstDataBill.addAll(lstDetail);
+		}
+		listDataModelDetail = new ListModelList(lstBillDetail);
+		gridBillsDetail.setModel(listDataModelDetail);
 
-    @Override
-    public void doAfterCompose(Component comp) throws Exception {
-        // TODO Auto-generated method stub
-        super.doAfterCompose(comp);
-        Long billId = txtBillID.getValue();
-        billsServices = (BillsServices) SpringUtil.getBean(SpringConstant.BILL_SERVICES);
-        List<BillViewDetail> lstData = billsServices.getDataView(billId);
-        List<BillsDetail> lstDetail = billsServices.getBillDetail(billId);
-        lstBillDetail = new ArrayList<>();
-        lstDataBill = new ArrayList<>();
-        if (lstData != null && !lstData.isEmpty()) {
-            lstBillDetail.addAll(lstData);
-        }
-        if (lstDetail != null && !lstDetail.isEmpty()) {
-            lstDataBill.addAll(lstDetail);
-        }
-        listDataModelDetail = new ListModelList(lstBillDetail);
-        gridBillsDetail.setModel(listDataModelDetail);
+		lstBill = billsServices.getAllData();
 
-        lstBill = billsServices.getAllData();
+		if (roleApprove.getValue() == 0) {
+			hideActionApprove();
+		}
+	}
 
-        if (roleApprove.getValue() == 0) {
-            hideActionApprove();
-        }
-    }
+	private void reload() {
+		Long billId = txtBillID.getValue();
+		List<BillViewDetail> lstData = billsServices.getDataView(billId);
+		lstBillDetail = new ArrayList<>();
 
-    private void reload() {
-        Long billId = txtBillID.getValue();
-        List<BillViewDetail> lstData = billsServices.getDataView(billId);
-        lstBillDetail = new ArrayList<>();
+		if (lstData != null && !lstData.isEmpty()) {
+			lstBillDetail.addAll(lstData);
+		}
 
-        if (lstData != null && !lstData.isEmpty()) {
-            lstBillDetail.addAll(lstData);
-        }
+		listDataModelDetail = new ListModelList<BillViewDetail>(lstBillDetail);
+		gridBillsDetail.setModel(listDataModelDetail);
+	}
 
-        listDataModelDetail = new ListModelList<BillViewDetail>(lstBillDetail);
-        gridBillsDetail.setModel(listDataModelDetail);
-    }
+	public void onAddStaff(ForwardEvent event) {
+		Vlayout rowSelected = (Vlayout) event.getOrigin().getTarget().getParent().getParent().getParent();
+		List<Component> lstCell = rowSelected.getChildren();
+		getDataInRow(lstCell, 1);
+	}
 
-    public void onAddStaff(ForwardEvent event) {
-        Vlayout rowSelected = (Vlayout) event.getOrigin().getTarget().getParent().getParent().getParent();
-        List<Component> lstCell = rowSelected.getChildren();
-        getDataInRow(lstCell, 1);
-    }
+	public void onApprove(ForwardEvent event) {
+		Vlayout rowSelected = (Vlayout) event.getOrigin().getTarget().getParent().getParent().getParent();
+		List<Component> lstCell = rowSelected.getChildren();
+		getDataInRow(lstCell, 2);
+	}
 
-    public void onApprove(ForwardEvent event) {
-        Vlayout rowSelected = (Vlayout) event.getOrigin().getTarget().getParent().getParent().getParent();
-        List<Component> lstCell = rowSelected.getChildren();
-        getDataInRow(lstCell, 2);
-    }
+	private void getDataInRow(List<Component> lstCell, int isApprove) {
 
-    private void getDataInRow(List<Component> lstCell, int isApprove) {
+		Component component;
 
-        Component component;
+		// ma phieu bom
+		component = lstCell.get(0);
+		if (component instanceof Textbox) {
+			final Textbox billID = (Textbox) component;
+			billDetailId = billID.getValue();
+		}
 
-        // ma phieu bom
-        component = lstCell.get(0);
-        if (component instanceof Textbox) {
-            final Textbox billID = (Textbox) component;
-            billDetailId = billID.getValue();
-        }
+		// TODO Auto-generated method stub
+		Map<String, Object> arguments = new HashMap();
+		arguments.put("billDetailID", billDetailId);
+		arguments.put("bill", getBill());
+		arguments.put("billDetai", getBillDetail(Long.valueOf(billDetailId)));
+		if (isApprove == 1) {
+			final Window windownUpload = (Window) Executions.createComponents("/manager/include/addStaff.zul",
+					windowViewBillDetail, arguments);
+			windownUpload.doModal();
+			windownUpload.setBorder(true);
+			windownUpload.setBorder("normal");
+			windownUpload.setClosable(true);
+			windownUpload.addEventListener(Events.ON_CLOSE, new EventListener<Event>() {
 
-        // TODO Auto-generated method stub
-        Map<String, Object> arguments = new HashMap();
-        arguments.put("billDetailID", billDetailId);
-        arguments.put("bill", getBill());
-        arguments.put("billDetai", getBillDetail(Long.valueOf(billDetailId)));
-        if (isApprove == 1) {
-            final Window windownUpload = (Window) Executions.createComponents("/manager/include/addStaff.zul",
-                    windowViewBillDetail, arguments);
-            windownUpload.doModal();
-            windownUpload.setBorder(true);
-            windownUpload.setBorder("normal");
-            windownUpload.setClosable(true);
-            windownUpload.addEventListener(Events.ON_CLOSE, new EventListener<Event>() {
+				@Override
+				public void onEvent(Event event) throws Exception {
+					reload();
+					windownUpload.detach();
 
-                @Override
-                public void onEvent(Event event) throws Exception {
-                    reload();
-                    windownUpload.detach();
+				}
+			});
+		} else if (isApprove == 2) {
+			Messagebox.show(Labels.getLabel("staff.quantity.comfirm.approve.message"), Labels.getLabel("comfirm"),
+					Messagebox.YES | Messagebox.NO, Messagebox.QUESTION, new EventListener() {
+						@Override
+						public void onEvent(Event e) {
+							if (Messagebox.ON_NO.equals(e.getName())) {
+								final Window windownUpload = (Window) Executions.createComponents(
+										"/manager/include/approveQuantity.zul", windowViewBillDetail, arguments);
+								windownUpload.doModal();
+								windownUpload.setBorder(true);
+								windownUpload.setBorder("normal");
+								windownUpload.setClosable(true);
+								windownUpload.addEventListener(Events.ON_CLOSE, new EventListener<Event>() {
 
-                }
-            });
-        } else if (isApprove == 2) {
-            Messagebox.show(Labels.getLabel("staff.quantity.comfirm.approve.message"), Labels.getLabel("comfirm"),
-                    Messagebox.YES | Messagebox.NO, Messagebox.QUESTION, new EventListener() {
-                @Override
-                public void onEvent(Event e) {
-                    if (Messagebox.ON_NO.equals(e.getName())) {
-                        final Window windownUpload = (Window) Executions.createComponents(
-                                "/manager/include/approveQuantity.zul", windowViewBillDetail, arguments);
-                        windownUpload.doModal();
-                        windownUpload.setBorder(true);
-                        windownUpload.setBorder("normal");
-                        windownUpload.setClosable(true);
-                        windownUpload.addEventListener(Events.ON_CLOSE, new EventListener<Event>() {
+									@Override
+									public void onEvent(Event event) throws Exception {
+										LOGGER.info("Reload");
+										reload();
+										windownUpload.detach();
 
-                            @Override
-                            public void onEvent(Event event) throws Exception {
-                                LOGGER.info("Reload");
-                                reload();
-                                windownUpload.detach();
+									}
+								});
+							} else if (Messagebox.ON_YES.equals(e.getName())) {
+								try {
+									BillsDetail billsDetail = new BillsDetail();
+									billsDetail.setBillDetailId(Long.valueOf(billDetailId));
+									billsDetail.setStatus(2);
 
-                            }
-                        });
-                    } else if (Messagebox.ON_YES.equals(e.getName())) {
-                        Messagebox.show(Labels.getLabel("staff.quantity.comfirm.approve.message.ok"),
-                                Labels.getLabel("comfirm"), Messagebox.OK, Messagebox.INFORMATION);
-                    }
-                }
-            });
+									billsServices.delete(billsDetail);
+								} catch (Exception e2) {
+									LOGGER.error(e2.getMessage(), e2);
+								}
+								Messagebox.show(Labels.getLabel("staff.quantity.comfirm.approve.message.ok"),
+										Labels.getLabel("comfirm"), Messagebox.OK, Messagebox.INFORMATION);
+							}
+						}
+					});
 
-        }
+		}
 
-    }
+	}
 
-    private BillsDetail getBillDetail(Long billDetailID) {
-        if (lstBillDetail != null && !lstBillDetail.isEmpty()) {
-            for (BillsDetail billsDetail : lstDataBill) {
-                if (billDetailID.equals(billsDetail.getBillDetailId())) {
-                    return billsDetail;
-                }
-            }
-        }
-        return null;
-    }
+	private BillsDetail getBillDetail(Long billDetailID) {
+		if (lstBillDetail != null && !lstBillDetail.isEmpty()) {
+			for (BillsDetail billsDetail : lstDataBill) {
+				if (billDetailID.equals(billsDetail.getBillDetailId())) {
+					return billsDetail;
+				}
+			}
+		}
+		return null;
+	}
 
-    private Bills getBill() {
-        Long billID = txtBillID.getValue();
-        if (lstBill != null && !lstBill.isEmpty()) {
-            for (Bills bills : lstBill) {
-                if (billID != null && billID.equals(bills.getBillID())) {
-                    return bills;
-                }
-            }
-        }
-        return null;
-    }
+	private Bills getBill() {
+		Long billID = txtBillID.getValue();
+		if (lstBill != null && !lstBill.isEmpty()) {
+			for (Bills bills : lstBill) {
+				if (billID != null && billID.equals(bills.getBillID())) {
+					return bills;
+				}
+			}
+		}
+		return null;
+	}
 
-    private void hideActionApprove() {
-        gridBillsDetail.renderAll();
-        Rows vrows = gridBillsDetail.getRows();
-        List<Component> lstRows = vrows.getChildren();
-        if (lstRows != null && !lstRows.isEmpty()) {
-            for (Component row : lstRows) {
-                if (row instanceof Row) {
-                    Component btnStaff = row.getFirstChild().getChildren().get(1).getFirstChild().getChildren().get(2);
-                    if (btnStaff instanceof A) {
-                        ((A) btnStaff).setVisible(false);
-                    }
-                    Component btnApprove = row.getFirstChild().getChildren().get(1).getFirstChild().getChildren().get(3);
-                    if (btnApprove instanceof A) {
-                        ((A) btnApprove).setVisible(false);
-                    }
-                }
-            }
-        }
-    }
+	private void hideActionApprove() {
+		gridBillsDetail.renderAll();
+		Rows vrows = gridBillsDetail.getRows();
+		List<Component> lstRows = vrows.getChildren();
+		if (lstRows != null && !lstRows.isEmpty()) {
+			for (Component row : lstRows) {
+				if (row instanceof Row) {
+					Component btnStaff = row.getFirstChild().getChildren().get(1).getFirstChild().getChildren().get(2);
+					if (btnStaff instanceof A) {
+						((A) btnStaff).setVisible(false);
+					}
+					Component btnApprove = row.getFirstChild().getChildren().get(1).getFirstChild().getChildren()
+							.get(3);
+					if (btnApprove instanceof A) {
+						((A) btnApprove).setVisible(false);
+					}
+				}
+			}
+		}
+	}
 
 }
